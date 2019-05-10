a68de3ae2138609ccb546afcb7df1bf9fd0be0d4
Tore Stenbock <tore.stenbock@blockpayd.com>
2019-05-10 17:14:40 +0200

    Updated with Infura nodes for deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,9 @@
 
+// Get all keys and ids from environment variables
+const HDWalletProvider = require("truffle-hdwallet-provider")
+const INFURA_API_KEY = process.env.INFURA_API_KEY
+const MNEMONIC = process.env.MNEMONIC
+
 module.exports = {
 
   networks: {
@@ -10,13 +15,16 @@ module.exports = {
     // },
 
     ropsten: {
-      host: "localhost",    // Location of Ropsten geth instance (LAN node)
-      port: 8545,           // Port of Ropsten geth instance (LAN node)
       network_id: 3,        // Ropsten's id
-      gas: 5500000,         // Ropsten has a lower block limit than mainnet
+
+      // host: "10.8.0.1",     // Location of Ropsten geth instance (Tunnel end-point)
+      // port: 8545,           // Port of Ropsten geth instance (LAN node)
+      // gas: 5500000,         // Ropsten has a lower block limit than mainnet
       // confirmations: 2,     // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,   // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      // timeoutBlocks: 200,   // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true      // Skip dry run before migrations? (default: false for public nets )
+
+      provider: new HDWalletProvider(MNEMONIC, 'https://ropsten.infura.io/v3/' + INFURA_API_KEY),
     },
   },
 
