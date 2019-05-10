18cb4c620ea3014cd855fb6c0af094dfefdb9467
Tore Stenbock <tore.stenbock@blockpayd.com>
2019-05-10 17:26:23 +0200

    Increased gas limits for Ropsten test network

--- truffle-config.js
+++ truffle-config.js
@@ -16,13 +16,13 @@ module.exports = {
 
     ropsten: {
       network_id: 3,        // Ropsten's id
-
-      // host: "10.8.0.1",     // Location of Ropsten geth instance (Tunnel end-point)
-      // port: 8545,           // Port of Ropsten geth instance (LAN node)
-      // gas: 5500000,         // Ropsten has a lower block limit than mainnet
+      gas: 5500000,         // Ropsten has a lower block limit than mainnet
       // confirmations: 2,     // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,   // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true      // Skip dry run before migrations? (default: false for public nets )
+
+      // host: "10.8.0.1",     // Location of Ropsten geth instance (Tunnel end-point)
+      // port: 8545,           // Port of Ropsten geth instance (LAN node)
 
       provider: new HDWalletProvider(MNEMONIC, 'https://ropsten.infura.io/v3/' + INFURA_API_KEY),
     },
